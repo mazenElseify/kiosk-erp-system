86e0878811354940746066125e8404f527fc88ca
mazenElseify <mazenelseify@gmail.com>
2025-10-21 16:35:26 +0300

    User model schema created

---
+++ src/models/Users.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength:[3, 'Username must be at least 3 characters long'],
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [8, 'Password must be at least 8 characters long']
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'manager', 'cashier'],
+        default: 'cashier'
+    },
+    phone: {
+        type: String,
+        trim: true,
+        required: [true, 'Phone number is required']
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true }
+);
+
+// Hash password before saving
+userSchema.pre('save' , async function (next) {
+    if (!this.isModified('password;')) return next();
+    
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
+
